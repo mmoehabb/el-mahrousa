c6c4f5668b3310d4468e517fd93f5ae2b246144e
M. E. Abdelsalam <mo.ehab150@gmail.com>
2026-05-13 02:50:47 +0300

    building the apk has been fixed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,19 @@
+import 'dotenv/config'
 import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.elmahrousa.app',
   appName: 'el-mahrousa',
   webDir: 'dist',
+  android: {
+    buildOptions: {
+      keystorePath: 'elmahrousa.jks',
+      keystorePassword: process.env.KEYSTORE_PASSWORD ?? '',
+      keystoreAlias: 'elmahrousa',
+      keystoreAliasPassword: process.env.KEYSTORE_ALIAS_PASSWORD ?? '',
+      releaseType: 'APK',
+    }
+  }
 }
 
 export default config
